4da8058b1426a31b9a42eb712d3342dcaf85fa70
jackyang2015 <byteyang@outlook.com>
2016-07-25 13:45:25 -0400

    add PureBean

--- todolist-core/src/main/java/io/github/todolist/core/domain/StateBean.java
+++ todolist-core/src/main/java/io/github/todolist/core/domain/StateBean.java
@@ -4,4 +4,12 @@ package io.github.todolist.core.domain;
  * Created by thanksgiving on 7/25/16.
  */
 public class StateBean {
+/*
+| id       | bigint(20)   | NO   | PRI | NULL    | auto_increment |
+| country  | varchar(128) | YES  |     | NULL    |                |
+| state    | varchar(128) | YES  |     | NULL    |                |
+| date     | date         | YES  |     | NULL    |                |
+| dayCount | bigint(20)   | NO   |     | NULL    |                |
+ */
+
 }
